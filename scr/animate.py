e8a182ffd7861fe690250f84e18174d9161d9ac8
leeacord <29208138+leeacord@users.noreply.github.com>
2020-11-21 20:37:39 +0800

    bug fix

--- scr/animate.py
+++ scr/animate.py
@@ -1,14 +1,6 @@
-import os
-
-import logging
-from tqdm import trange
-import imageio
-from scipy.spatial import ConvexHull
-from frames_dataset import PairedDataset
-from train import load_ckpt
 import numpy as np
-
-# from sync_batchnorm import DataParallelWithCallback
+import paddle
+from scipy.spatial import ConvexHull
 
 
 def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale=False,
@@ -19,16 +11,14 @@ def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale
         adapt_movement_scale = np.sqrt(source_area) / np.sqrt(driving_area)
     else:
         adapt_movement_scale = 1
-
+        
     kp_new = {k: v for k, v in kp_driving.items()}
 
     if use_relative_movement:
         kp_value_diff = (kp_driving['value'] - kp_driving_initial['value'])
         kp_value_diff *= adapt_movement_scale
         kp_new['value'] = kp_value_diff + kp_source['value']
-
         if use_relative_jacobian:
-            jacobian_diff = np.matmul(kp_driving['jacobian'].numpy(), np.linalg.inv(kp_driving_initial['jacobian'].numpy()))
-            kp_new['jacobian'] = dygraph.to_variable(np.matmul(jacobian_diff, kp_source['jacobian'].numpy()))
-
+            jacobian_diff = paddle.matmul(kp_driving['jacobian'], paddle.inverse(kp_driving_initial['jacobian']))
+            kp_new['jacobian'] = paddle.matmul(jacobian_diff, kp_source['jacobian'])
     return kp_new
