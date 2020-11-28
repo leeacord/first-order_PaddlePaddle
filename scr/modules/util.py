ae02b710be4b2c9a7ab52b102a26d0c61d232689
leeacord <29208138+leeacord@users.noreply.github.com>
2020-11-28 23:19:04 +0800

    bug fix stop bp in AntiAliasInterpolation2d

--- scr/modules/util.py
+++ scr/modules/util.py
@@ -186,45 +186,49 @@ class Hourglass(paddle.nn.Layer):
         return self.decoder(self.encoder(x))
 
 
-class AntiAliasInterpolation2d(paddle.nn.Layer):
+class AntiAliasInterpolation2d(nn.Layer):
     """
     Band-limited downsampling, for better preservation of the input signal.
     """
+    
     def __init__(self, channels, scale):
         super(AntiAliasInterpolation2d, self).__init__()
         sigma = (1 / scale - 1) / 2
         kernel_size = 2 * round(sigma * 4) + 1
         self.ka = kernel_size // 2
         self.kb = self.ka - 1 if kernel_size % 2 == 0 else self.ka
-
+        
         kernel_size = [kernel_size, kernel_size]
         sigma = [sigma, sigma]
         # The gaussian kernel is the product of the
         # gaussian function of each dimension.
         kernel = 1
-        meshgrids = paddle.meshgrid([paddle.arange(size, dtype='float32') for size in kernel_size])
+        meshgrids = paddle.meshgrid(
+            [paddle.arange(size, dtype='float32') for size in kernel_size])
         for size, std, mgrid in zip(kernel_size, sigma, meshgrids):
             mean = (size - 1) / 2
             kernel *= paddle.exp(-(mgrid - mean) ** 2 / (2 * std ** 2 + 1e-9))
-
+        
         # Make sure sum of values in gaussian kernel equals 1.
         kernel = kernel / paddle.sum(kernel)
         # Reshape to depthwise convolutional weight
-        kernel = kernel.reshape((1, 1, *kernel.shape))
-        kernel = kernel.tile((channels, *((1,)*(len(kernel.shape)-1))))  # [1, 1, *kernel.shape] -> [channels, 1, *kernel.shape]
-        self.kernel_attr = paddle.ParamAttr(initializer=paddle.nn.initializer.Assign(kernel), trainable=False)
+        kernel = kernel.reshape([1, 1, *kernel.shape])
+        kernel = paddle.tile(kernel, [channels, *[1] * (kernel.dim() - 1)])
+        
+        self.register_buffer('weight', kernel)
         self.groups = channels
         self.scale = scale
-        self.conv = nn.Conv2D(channels, channels, kernel_size=kernel.shape[-1], groups=self.groups,
-                              weight_attr=self.kernel_attr,
-                              bias_attr=False
-                              )
-        self.conv.weight.set_value(kernel)
-
+    
     def forward(self, input):
         if self.scale == 1.0:
             return input
-        out = F.pad(input, [self.ka, self.kb, self.ka, self.kb], mode='constant')
-        out = self.conv(out)
-        out = F.interpolate(out, scale_factor=self.scale, mode='NEAREST', align_corners=False)
+        
+        out = F.pad(input, [self.ka, self.kb, self.ka, self.kb])
+        out = F.conv2d(out, weight=self.weight, groups=self.groups)
+        out.stop_gradient = False
+        # out = F.interpolate(out, scale_factor=[self.scale, self.scale])
+        inv_scale = 1 / self.scale
+        int_inv_scale = int(inv_scale)
+        assert (inv_scale == int_inv_scale)
+        out = out[:, :, ::int_inv_scale, ::int_inv_scale]
         return out
